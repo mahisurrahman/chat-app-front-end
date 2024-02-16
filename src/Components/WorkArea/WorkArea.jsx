6dc77b17a2deac7a2d30267df585fc22aeb7cf1e
mahisurrahman <mahisur.rahman.001@gmail.com>
2024-02-17 03:29:31 +0600

    Work Space Structure Done

--- src/Components/WorkArea/WorkArea.jsx
+++ src/Components/WorkArea/WorkArea.jsx
@@ -1,12 +1,19 @@
 import UserAreaUserCreds from "../WorkAreaUserCreds/UserAreaUserCreds";
 
-
 const WorkArea = () => {
-    return (
-        <div className="bg-zinc-950 w-full h-full pr-[0.5vw] py-[1vh] flex flex-col">
-            <UserAreaUserCreds></UserAreaUserCreds>
-        </div>
-    );
+  return (
+    <div className="bg-zinc-950 w-full h-full pr-[0.5vw] py-[1vh] grid grid-cols-1 grid-rows-9">
+      <div className="row-span-1">
+        <UserAreaUserCreds></UserAreaUserCreds>
+      </div>
+      <div className="row-span-7">
+
+      </div>
+      <div className="row-span-1">
+
+      </div>
+    </div>
+  );
 };
 
 export default WorkArea;
