c2e56d85970e39d4a9423d323749632507e06073
mahisurrahman <mahisur.rahman.001@gmail.com>
2024-02-16 06:52:43 +0600

    Did a bit of Customization

--- src/Layouts/MainLayout/MainLayout.jsx
+++ src/Layouts/MainLayout/MainLayout.jsx
@@ -3,8 +3,8 @@ import WorkArea from "../../Components/WorkArea/WorkArea";
 
 const MainLayout = () => {
   return (
-    <div className="bg-gray-800 w-screen h-screen px-10 py-10">
-      <div className="grid grid-cols-3 h-full">
+    <div className="bg-gray-800 w-screen h-screen px-5 py-5">
+      <div className="grid grid-cols-3 gap-2 h-full">
         <div className="col-span-1">
             <Sidebar></Sidebar>
         </div>
