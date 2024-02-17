f80bea6b91dd6cf577d3cc8544bc5ee98de20809
mahisurrahman <mahisur.rahman.001@gmail.com>
2024-02-17 23:08:57 +0600

    Added dynamic Scrollbar to the Chats

--- src/Components/WorkAreaChats/WorkAreaChats.jsx
+++ src/Components/WorkAreaChats/WorkAreaChats.jsx
@@ -4,7 +4,7 @@ import MessageSelf from "../MessageSelf/MessageSelf";
 
 const WorkAreaChats = () => {
     return (
-        <div className="h-full px-[1vw] py-[2vh] border border-indigo-600 text-zinc-100 rounded-lg font-poppins">
+        <div className="h-full px-[1vw] py-[2vh] border border-indigo-600 text-zinc-100 rounded-lg font-poppins overflow-y-auto max-h-[75vh]">
             <MessageOthers></MessageOthers>
             <MessageSelf></MessageSelf>
             <MessageOthers></MessageOthers>
