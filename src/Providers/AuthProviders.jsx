674ba089b0e098470821990a794353118d28e515
mahisurrahman <mahisur.rahman.001@gmail.com>
2024-02-18 20:12:25 +0600

    Added the Auth Providers Context

--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -1,19 +1,69 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { app } from "../Firebase/firebase.config";
-import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProviders = ({children}) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    //Create User//
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    //Login User//
+    const LoginUser = (email, password) =>{ 
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    //Logout User//
+    const logOut = () =>{ 
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    //Updating User Info
+    const updateUserInfo = (name, photo)=>{
+        setLoading(true);
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photo,
+          });
+    }
+
+    //Google Sign In
+    const googleLogin = ()=>{
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+    
+    //Holding the User State
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return ()=>{
+            return unsubscribe();
+        }
+    },[])
+
+
     const userInfo = {
         user, 
         setUser,
         loading,
-        setLoading
+        setLoading,
+        createUser,
+        LoginUser,
+        updateUserInfo,
+        logOut,
+        googleLogin
     }
 
     return (
