2432a996f505747c590c78ae4d53bad793b702f0
mahisurrahman <mahisur.rahman.001@gmail.com>
2024-02-20 00:45:38 +0600

    Completed Responsive

--- src/Components/SidebarNav/SidebarNav.jsx
+++ src/Components/SidebarNav/SidebarNav.jsx
@@ -11,7 +11,7 @@ const SidebarNav = () => {
   return (
     <div className="mb-[2vh] px-[1vw] py-[8vh] md:py-[2vh] flex flex-col md:flex-row gap-40 md:gap-0 justify-start md:justify-between items-center md:h-15 text-zinc-100 rounded-lg text-[10vw] md:text-[2vw]">
       <div>
-          <FaRegUserCircle className="duration-700 text-purple-400 hover:text-red-600 hover:cursor-pointer hover:duration-700"></FaRegUserCircle>
+          <FaRegUserCircle className="text-purple-400"></FaRegUserCircle>
       </div>
       <div className="flex flex-col md:flex-row gap-10 md:gap-4">
         <Link to="/app/welcome">
--- src/Pages/WelcomePage/WelcomePage.jsx
+++ src/Pages/WelcomePage/WelcomePage.jsx
@@ -4,7 +4,7 @@ import wizcatschatsimage from "../../assets/chatIcon.png";
 const WelcomePage = () => {
   return (
       <div className="bg-transparent w-full h-full px-[1vw] py-[2vh]">
-        <div className="flex flex-col justify-center items-center w-full h-full border-2 rounded-lg border-indigo-800">
+        <div className="flex flex-col justify-center items-center w-full h-full rounded-lg">
           <h1 className="text-center font-avent-pro text-[10vw] md:text-[4vw] text-purple-500">
             Hi! Welcome to WizCats Chats
           </h1>
