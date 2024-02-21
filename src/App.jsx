70762ba1431b04ce1c55a85c1a28dc54d2d22c02
mahisurrahman <mahisur.rahman.001@gmail.com>
2024-02-22 02:59:37 +0600

    Solved a Few Bugs

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,11 @@ import WorkArea from "./Components/WorkArea/WorkArea";
 import OnlineUsers from "./Components/OnlineUsers/OnlineUsers";
 import Groups from "./Components/Groups/Groups";
 import CreateGroups from "./Pages/CreateGroupsPage/CreateGroups";
-import SignUp from "./Pages/SignUp/SignUp";
-
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Login></Login>} />
-        <Route path="/signup" element={<SignUp></SignUp>}/>
         <Route path="app" element={<MainLayout></MainLayout>}>
           <Route path="welcome" element={<WelcomePage></WelcomePage>}></Route>
           <Route path="chat" element={<WorkArea></WorkArea>}></Route>
